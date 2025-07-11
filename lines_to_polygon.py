3a3443b9e111eec70af9d5a8ae751fe083247ee1
MercerRAÄ <167416139+mercerraa@users.noreply.github.com>
2025-07-11 09:31:27 +0200

    Update lines_to_polygon.py
    Small alterations to how the plugins ineracts with the user. The old version used a message box that needed clicking away. This uses message bar instead.

--- lines_to_polygon.py
+++ lines_to_polygon.py
@@ -1,5 +1,6 @@
 from qgis.PyQt.QtWidgets import QAction, QMessageBox
 from qgis.core import QgsProject
+from qgis.utils import iface
 
 # Import your processing logic here
 from .polygon_from_lines import polygonise
@@ -10,7 +11,8 @@ class LinesToPolygon:
         self.plugin_action = None
 
     def initGui(self):
-        self.plugin_action = QAction("Run Lines to polygon", self.iface.mainWindow())
+        self.plugin_action = QAction("Lines to polygon", self.iface.mainWindow())
+        #self.action = QAction(QIcon(os.path.join( os.path.dirname(__file__), 'icon.png' )), "Reverse layer order", self.iface.mainWindow())
         self.plugin_action.triggered.connect(self.run)
         self.iface.addPluginToMenu("&Lines to polygon", self.plugin_action)
         self.iface.addToolBarIcon(self.plugin_action)
@@ -28,6 +30,9 @@ class LinesToPolygon:
         try:
             polygonise()
 
-            QMessageBox.information(None, "Success", "Plugin executed successfully.")
+            #QMessageBox.information(None, "Success", "Plugin executed successfully.")
+            iface.messageBar().pushMessage("Success", "Lines to Polygon complete. Check for layers", level=Qgis.Success, duration=3)
         except Exception as e:
-            QMessageBox.critical(None, "Error", str(e))
+            #QMessageBox.critical(None, "Error", str(e))
+            message = f'Error: {str(e)}'
+            iface.messageBar().pushMessage("Warning", message, level=Qgis.Warning, duration=3)
